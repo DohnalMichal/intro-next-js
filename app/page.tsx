c0be46b1adc071a1f6085586a26e428ddb485599
DohnalMichal <michal.dohnal7@gmail.com>
2024-02-18 13:31:21 +0100

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,30 @@
+import Link from 'next/link'
+
 const Home = () => {
-  return <div className="text-3xl">Home</div>
+  return (
+    <div>
+      <h1 className="text-3xl">Home</h1>
+      <p className="mb-4">
+        This is a Next.js application that leverages Prisma for database
+        interactions and Next Route handlers and new server actions. The app
+        provides API documentation using a dynamic routing structure and
+        showcases a combination of Tailwind CSS for layouts and
+        Styled-Components for individual components.
+      </p>
+      <h2 className="text-2xl">Links</h2>
+      <ul>
+        <li>
+          <Link href="/docs">API Documentation</Link>
+        </li>
+        <li>
+          <Link href="/api/todo">Todos</Link>
+        </li>
+        <li>
+          <Link href="/api/todo">Todo API</Link>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default Home
